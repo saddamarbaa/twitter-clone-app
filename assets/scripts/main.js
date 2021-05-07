cf6a6e9655bf1e2a94655d85a25c0601c1b54afb
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-05-07 18:33:56 +0700

    ADD: Setup functions  structure

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,7 +5,7 @@ const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 const rootElement = document.documentElement;
 const navIcon = document.getElementById("toggle");
 
-// Functions
+// Scroll to top logic
 const scrollToTop = () => {
 	// Scroll to top logic
 	rootElement.scrollTo({
@@ -21,3 +21,26 @@ const toggleBetweenClass = () => {
 // EventListener
 scrollToTopBtn.addEventListener("click", scrollToTop);
 navIcon.addEventListener("click", toggleBetweenClass);
+
+const URL = "http://localhost:3000/tweets";
+
+// Retrive Twitter Data from API
+const getTwitterData = () => {};
+
+//  Save the next page data
+const saveNextPage = (metadata) => {};
+
+// Handle when a user clicks on a trend
+const selectTrend = (e) => {};
+
+// Set the visibility of next page based on if there is data on next page
+const nextPageButtonVisibility = (metadata) => {};
+
+// Build Tweets HTML based on Data from API
+const buildTweets = (tweets, nextPage) => {};
+
+//  Build HTML for Tweets Images
+const buildImages = (mediaList) => {};
+
+// Build HTML for Tweets Video
+const buildVideo = (mediaList) => {};
