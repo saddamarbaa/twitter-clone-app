f2a0e99b0ef8270bb078ed9acdfd1826fdaffd01
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-05-17 16:26:51 +0700

    feat:  adding Showing tweets based on API Data

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,7 @@
 
 // Selectors
 const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+const URL = "http://localhost:3000/api/tweets";
 
 // Scroll to top logic
 const scrollToTop = () => {
@@ -12,22 +13,27 @@ const scrollToTop = () => {
 	});
 };
 
-// EventListener
-scrollToTopBtn.addEventListener("click", scrollToTop);
+const onEnter = (event) => {
+	if (event.key === "Enter") {
+		getTwitterData();
+	}
+};
 
 // Retrieve Twitter Data from API
 const getTwitterData = () => {
-	const URL = "http://localhost:3000/api/tweet/search?q=coding&count=15";
-	fetch(URL)
+	const query = document.getElementById("user-search-input").value;
+	const encodedQuery = encodeURIComponent(query);
+	const fulUrl = `${URL}/search?q=${encodedQuery}&count=10`;
+	if (!query) return;
+
+	fetch(fulUrl)
 		.then((response) => {
 			return response.json();
 		})
 		.then((data) => {
-			console.log(data.result.statuses);
+			buildTweets(data.result.statuses);
 		})
-		.catch((error) => {
-			console.log(error);
-		});
+		.catch((error) => {});
 };
 
 //  Save the next page data
@@ -40,7 +46,49 @@ const selectTrend = (e) => {};
 const nextPageButtonVisibility = (metadata) => {};
 
 // Build Tweets HTML based on Data from API
-const buildTweets = (tweets, nextPage) => {};
+const buildTweets = (tweets, nextPage) => {
+	tweets.map((tweet) => {
+		let twitterContentEl = `
+		<div class='tweets-list-container'>
+			<div class='tweets-user-info'>
+				<div class='image'></div>
+				<div class='info'>
+					<div class='name'>Coach | Saddam Arbaa</div>
+					<div class='twitter-name'>@ArbaaSaddam</div>
+				</div>
+
+				<div class='tweets-user-info-add'>
+					<i class='fas fa-ellipsis-h' title='More'></i>
+				</div>
+			</div>
+
+			<div class='tweets-text-container'>
+				<p>
+				${tweet.full_text}
+				</p>
+			</div>
+
+			<div class='tweets-images-container'>
+				<div class='img'>
+					<img src='./assets/img/s.png' alt='img' />
+				</div>
+			</div>
+			<div class='tweets-date-container'>20 hours age</div>
+			<div class='tweets-addtional-container'>
+				<i title='Reply' class='far fa-comments reply'></i>
+				<i title='share' class='fas fa-retweet'></i>
+				<i title='Like' class='fas fa-heart'>
+					&nbsp; &nbsp; ${tweet.user.followers_count}
+				</i>
+				<i title='Share' class='fas fa-share-alt'></i>
+			</div>
+		</div>
+		`;
+		document.getElementById(
+			"tweets-list-container",
+		).innerHTML += twitterContentEl;
+	});
+};
 
 //  Build HTML for Tweets Images
 const buildImages = (mediaList) => {};
@@ -48,6 +96,5 @@ const buildImages = (mediaList) => {};
 // Build HTML for Tweets Video
 const buildVideo = (mediaList) => {};
 
-window.onload = () => {
-	getTwitterData();
-};
+// EventListener
+scrollToTopBtn.addEventListener("click", scrollToTop);
